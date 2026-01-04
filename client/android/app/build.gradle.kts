7e84c5ba0570fdfe2cd5c8fe6b41ada0fd289482
Your Name <you@example.com>
2026-01-05 01:17:51 +0530

    Fix: Add INTERNET permission and cleartext traffic support

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.mobile_proxy_client"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "26.1.10909125"
+    ndkVersion = "27.3.13750724"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
